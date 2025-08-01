2a26a8b4ad28d001c72bbff45b1bc1059efe80db
shubhamnar93 <shubhamnarnolia925@gmail.com>
2025-08-01 12:51:36 +0530

    changes for prod

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,10 +1,10 @@
 import { Editor } from "./editor";
 import { Toolbar } from "./toolbar";
 
-interface DocumentIdPageProps {
-  params: Promise<{ documentId: string }>;
-}
-const DocumentId = async ({ params }: DocumentIdPageProps) => {
+// interface DocumentIdPageProps {
+//   params: Promise<{ documentId: string }>;
+// }
+const DocumentId = async (/*{ params }: DocumentIdPageProps*/) => {
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
       <Toolbar />
