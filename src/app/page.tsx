4a9ffac66083691bae755e661afabc5564f2bf49
shubhamnar93 <shubhamnarnolia925@gmail.com>
2025-08-01 12:53:52 +0530

    changing for prod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import {Button} from "@/components/ui/button";
-import Link from "next/link"
-const Home=()=>{
-    return (
-        <div className="flex min-h-screen items-center justify-center">
-            Click <Link href="/documents/123">&nbsp;<span className="text-blue-500">here</span>&nbsp;</Link> to go to document id        
-        </div>
-    )
-}
-export default Home
+import Link from "next/link";
+const Home = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      Click{" "}
+      <Link href="/documents/123">
+        &nbsp;<span className="text-blue-500">here</span>&nbsp;
+      </Link>{" "}
+      to go to document id
+    </div>
+  );
+};
+export default Home;
