ca64f983f89a93adb7fe958810cbf7bc75c6bfc0
shubhamnar93 <shubhamnarnolia925@gmail.com>
2025-08-03 12:46:32 +0530

    fixing ruler bug

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -55,12 +55,9 @@ export const Ruler = () => {
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
       onMouseLeave={handleMouseUp}
-      className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden"
+      className="w-[816px] mx-auto h-6 border-b border-gray-300 flex items-end relative select-none print:hidden"
     >
-      <div
-        id="ruler-container"
-        className="max-w-[816px] h-full mx-auto w-full relative"
-      >
+      <div id="ruler-container" className=" h-full w-full relative">
         <Marker
           position={leftMargin}
           isLeft={true}
