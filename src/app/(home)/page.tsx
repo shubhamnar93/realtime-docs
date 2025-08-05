f654a1079b50bcd70399b1ceff7d060c45e23a39
shubhamnar93 <shubhamnarnolia925@gmail.com>
2025-08-05 12:26:07 +0530

    adding template gallery component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,14 @@
-import Link from "next/link";
 import { Navbar } from "./navbar";
+import { TemplatesGallery } from "./templates-gallery";
 
 const Home = () => {
   return (
-    <div className="flex flex=col min-h-screen ">
+    <div className="flex flex-col min-h-screen">
       <div className="fixed top-0 p-4 left-0 right-0 z-10 h-16 bg-white">
         <Navbar />
       </div>
       <div className="mt-16">
-        Click
-        <Link href="/documents/123">
-          &nbsp;<span className="text-blue-500">here</span>&nbsp;
-        </Link>
-        to go to document id
+        <TemplatesGallery />
       </div>
     </div>
   );
---
+++ src/constants/templates.ts
@@ -0,0 +1,37 @@
+export const templates = [
+  {
+    id: "blank",
+    label: "Blank Document",
+    imageUrl: "/blank-document.svg",
+  },
+  {
+    id: "software-proposal",
+    label: "Software Development Proposal",
+    imageUrl: "/software-proposal.svg",
+  },
+  {
+    id: "business-letter",
+    label: "Business Letter",
+    imageUrl: "/business-letter.svg",
+  },
+  {
+    id: "cover-letter",
+    label: "Cover Letter",
+    imageUrl: "/cover-letter.svg",
+  },
+  {
+    id: "letter",
+    label: "letter",
+    imageUrl: "/letter.svg",
+  },
+  {
+    id: "resume",
+    label: "resume",
+    imageUrl: "/resume.svg",
+  },
+  {
+    id: "project-proposal",
+    label: "project proposal",
+    imageUrl: "/project-proposal.svg",
+  },
+];
